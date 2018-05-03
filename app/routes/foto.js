e1a6eb29bf0a474d9d5b9a7f82b25baa8eaa82fa
LR-Developer GitHub <lr.developer@outlook.com>
2018-05-03 00:44:05 -0300

    Aula 05: * Criando listarPorId (rota e api).

--- app/routes/foto.js
+++ app/routes/foto.js
@@ -3,4 +3,14 @@ module.exports = function(app) {
     var api = app.api.foto;
 
     app.get('/v1/fotos', api.listar);
+    app.get('/v1/fotos/:id', api.listarPorId);
+
+    app.get('/v1/fotos/:id/tipo/:idTipo', function(req, res) {
+
+        var coringa01 = req.params.id;
+        var coringa02 = req.params.idTipo;
+
+        console.log('coringa01: ' + coringa01);
+        console.log('coringa02: ' + coringa02);
+     });
 };
