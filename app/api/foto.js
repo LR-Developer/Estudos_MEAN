da9ca8f50603fc97966b87f7c110308f6dd3d7e0
LR-Developer <lr.developer@outlook.com>
2018-05-04 19:36:57 -0300

    Aula 10: * Criação da model foto; * Implemetação do listar todos da api.

--- app/api/foto.js
+++ app/api/foto.js
@@ -1,56 +1,37 @@
-var api = {};
-var CONTADOR = 2;
+var mongoose = require('mongoose');
 
-var fotos = [
-    {_id: 1, titulo: 'Leão', url:'http://www.fundosanimais.com/Minis/leoes.jpg' },
-    {_id: 2, titulo: 'Leão 2', url:'http://www.fundosanimais.com/Minis/leoes.jpg' }
-];
+module.exports = function(app) {
 
-api.listar = function(req, res) {
-    
-    res.json(fotos);
-};
+    var api = {};
+    var model = mongoose.model('Foto');
 
-api.listarPorId = function(req, res) {
+    api.listar = function(req, res) {
+        
+        model.find()
+            .then(function(fotos) {
+                res.json(fotos);
+            },
+            function(error) {
+                console.log(error);
+                res.sendStatus(500);
+            });
+    };
 
-    var foto = fotos.find(function(foto) {
-        return foto._id == req.params.id;
-    });
+    api.listarPorId = function(req, res) {
 
-    res.json(foto);
-};
+    };
 
-api.incluir = function(req, res) {
+    api.incluir = function(req, res) {
 
-    var foto = req.body;
-    foto._id = ++CONTADOR;
-    fotos.push(foto);
+    };
 
-    res.json(foto);
-};
+    api.alterarPorId = function(req, res) {
 
-api.alterarPorId = function(req, res) {
+    };
 
-    var foto = req.body;
-    var idFoto = req.params._id;
+    api.deletarPorId = function(req, res) {
 
-    var indice = fotos.findIndex(function(foto  ) {
-        return fotos._id == idFoto;
-    });
+    };
 
-    fotos[indice] = foto;
-
-    res.sendStatus(200);
-};
-
-api.deletarPorId = function(req, res) {
-
-    fotos = fotos.filter(function(foto) {
-
-        return foto._id != req.params.id;
-    });
-
-    res.sendStatus(204);
-};
-
-module.exports = api;
+    return api;
+}
