af95946c752d066f2fdb45cb16612ba48653728a
LR-Developer <lr.developer@outlook.com>
2018-05-04 20:24:04 -0300

    Aula 11: * Finalizando api (create, delete e update).

--- app/api/foto.js
+++ app/api/foto.js
@@ -19,18 +19,53 @@ module.exports = function(app) {
 
     api.listarPorId = function(req, res) {
 
+        model.findById(req.params.id)
+            .then(function(foto){
+                if (!foto) throw Error('Foto não encontrada.')
+                res.json(foto);
+            },
+            function(error){
+                console.log(error);
+                res.sendStatus(404);
+            })
     };
 
     api.incluir = function(req, res) {
 
+        model.create(req.body)
+            .then(function(foto){
+                res.json(foto);
+                res.sendStatus(200);
+            },
+            function(error){
+                console.log(error);
+                res.sendStatus(500);
+            });
     };
 
     api.alterarPorId = function(req, res) {
 
+        model.findByIdAndUpdate(req.params.id, req.body)
+            .then(function(foto){
+                res.json(foto);
+                res.sendStatus(200);
+            },
+            function(error){
+                console.log(error);
+                res.sendStatus(500);
+            });
     };
 
     api.deletarPorId = function(req, res) {
 
+        model.remove({_id: req.params.id})
+            .then(function() {
+                res.sendStatus(204);
+            },
+            function(error) {
+                console.log(error);
+                res.sendStatus(500);
+            })
     };
 
     return api;
